608e2517c1843a0414186d29ca8b7b083c98f435
alik0002 <nelson.alikou-dongmo@etudiant.univ-reims.fr>
2021-01-15 19:19:46 +0100

    Mise à jour footer et début ajout BD

---
+++ src/Entity/Messages.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MessagesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=MessagesRepository::class)
+ */
+class Messages
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $SendAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getname(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setname(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getcontent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setcontent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getSendAt(): ?\DateTimeInterface
+    {
+        return $this->SendAt;
+    }
+
+    public function setSendAt(\DateTimeInterface $SendAt): self
+    {
+        $this->SendAt = $SendAt;
+
+        return $this;
+    }
+}
